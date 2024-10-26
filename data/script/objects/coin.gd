13b48c366d57a1e8d529336c23bbf4cf11716428
Surihix <neweramysterymailer@gmail.com>
2024-10-26 22:54:17 +0530

    change object node paths

--- data/script/objects/coin.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@onready var animation_player = $AnimationPlayer
-
-func _on_body_entered(_body):
-	AudioManager.playCoinSound()
-	GameManager.coinsCollected += 1
-	(GameManager.currentStageUiTxtNode).text = "x" + str(GameManager.coinsCollected)
-	animation_player.play("Coin_pickUp")
