ed46eb8886782591dcdb823f4b5b168c56071f47
Surihix <neweramysterymailer@gmail.com>
2024-10-29 03:23:21 +0530

    stage 5 initial prep

--- data/script/zone/stage_5.gd
+++ data/script/zone/stage_5.gd
@@ -10,3 +10,11 @@ func _ready():
 	(GameManager.currentStageUiTxtNode).text = "x" + str(GameManager.coinsCollectedInPrevStages)
 	GameManager.coinsCollected = GameManager.coinsCollectedInPrevStages
 	GameManager.canMove = true
+
+
+func _on_exit_body_entered(_body):
+	print("exited_stage_4")
+	GameManager.coinsCollectedInPrevStages = GameManager.coinsCollected
+	GameManager.canMove = false
+	AudioManager.isTrackSet = false
+	get_tree().call_deferred("change_scene_to_file", "res://data/zone/end_screen.tscn")
