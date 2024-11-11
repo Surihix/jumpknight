b08c121fff8de6abd907c051eefb0174f2eee2e3
Surihix <neweramysterymailer@gmail.com>
2024-11-11 23:43:39 +0530

    changed how shader works

--- data/script/shader/shader_control.gd
+++ data/script/shader/shader_control.gd
@@ -1,18 +1,10 @@
 extends CanvasLayer
 
 func _on_visibility_changed():
-	var canvas_layer = get_tree().current_scene.get_node("ShaderLayer")
-	canvas_layer.offset.x = 0
-	canvas_layer.offset.y = 0
+	var sizeX = get_viewport().size.x
+	var sizeY = get_viewport().size.y
 	
-	var sizeX = 1152
-	var sizeY = 648
-	
-	var mesh_instance = canvas_layer.get_node("MeshInstance2D")
-	var quad_mesh = mesh_instance.mesh as QuadMesh
+	var quad_mesh = $MeshInstance2D.mesh as QuadMesh
 	
 	quad_mesh.size = Vector2(sizeX, sizeY)
 	quad_mesh.center_offset = Vector3(sizeX/2, sizeY/2, 0.0)
-	
-	var material = mesh_instance as CanvasItem
-	material = material.material
